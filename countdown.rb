a2a2d03b392578bd25696b696dbe8d141d7d25ca
JazzPants <leighton.l14@gmail.com>
2022-01-19 00:57:31 +1100

    Done!

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+    while number >= 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+        sleep(1)
+    end
+    puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep(5)
